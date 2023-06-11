b27f14c8e2b9923802326f37f7ed18629230e311
Jim Falzone <54333603+jimfalzone@users.noreply.github.com>
2023-06-11 17:52:27 -0600

    fix hamburger menu

--- components/navigation.js
+++ components/navigation.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 const Navigation = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -11,9 +13,6 @@ const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        {isMenuOpen ? (
-        <Link href="/">home</Link>
-        ) : null}
         <button
           className="navbar-toggler"
           type="button"
@@ -21,12 +20,35 @@ const Navigation = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div
-          className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}
-        >
+        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}>
           <ul className="navbar-nav">
+            {isMenuOpen && (
+              <li className="nav-item">
+                <Link href="/">
+                  <span
+                    className={
+                      router.asPath === '/'
+                        ? 'nav-link active'
+                        : 'nav-link'
+                    }
+                  >
+                    Home
+                  </span>
+                </Link>
+              </li>
+            )}
             <li className="nav-item">
-            <Link href="posts/first-post">first post</Link>
+              <Link href="/posts/first-post">
+                <span
+                  className={
+                    router.asPath === '/posts/first-post'
+                      ? 'nav-link active'
+                      : 'nav-link'
+                  }
+                >
+                  First Post
+                </span>
+              </Link>
             </li>
             {/* Add more navigation links as needed */}
           </ul>
