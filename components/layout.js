83a95be62f9f2d52348a72dca3c59d173b049735
Jim Falzone <54333603+jimfalzone@users.noreply.github.com>
2023-06-11 22:24:00 -0600

    add logo to navbar

--- components/layout.js
+++ components/layout.js
@@ -9,63 +9,63 @@ const name = 'Medical Claims Physician Reviewer';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>                               
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-        <meta                  
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle,
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
                 <Navigation />
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/docimage.png"
-              className={utilStyles.borderCircle}
-              height={240}
-              width={180}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/docimage.png"
-                className={utilStyles.borderCircle}
-                height={160}
-                width={120}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/docimage.png"
+                            className={utilStyles.borderCircle}
+                            height={240}
+                            width={180}
+                            alt=""
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <Image
+                                priority
+                                src="/images/docimage.png"
+                                className={utilStyles.borderCircle}
+                                height={160}
+                                width={120}
+                                alt=""
+                            />
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/" className={utilStyles.colorInherit}>
+                                {name}
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">← Back to home</Link>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
--- components/navigation.js
+++ components/navigation.js
@@ -1,61 +1,63 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navigation = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const router = useRouter();
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const router = useRouter();
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={toggleMenu}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}>
-          <ul className="navbar-nav">
-            {isMenuOpen && (
-              <li className="nav-item">
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-white container-fluid">
+            <div className="container-fluid">
                 <Link href="/">
-                  <span
-                    className={
-                      router.asPath === '/'
-                        ? 'nav-link active'
-                        : 'nav-link'
-                    }
-                  >
-                    Home
-                  </span>
+                    <div className="navbar-brand">
+                        <Image
+                            priority
+                            src="/images/logo.png"
+                            height={120}
+                            width={120}
+                            alt=""
+                        />
+                    </div>
                 </Link>
-              </li>
-            )}
-            <li className="nav-item">
-              <Link href="/posts/first-post">
-                <span
-                  className={
-                    router.asPath === '/posts/first-post'
-                      ? 'nav-link active'
-                      : 'nav-link'
-                  }
-                >
-                  First Post
-                </span>
-              </Link>
-            </li>
-            {/* Add more navigation links as needed */}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+                <div className="">
+                    <Link href="/">
+                        <span className="navbar-brand">Home</span>
+                    </Link>
+                </div>
+            </div>
+            <button
+                className="navbar-toggler"
+                type="button"
+                onClick={toggleMenu}
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}>
+                <ul className="navbar-nav mx-auto">
+                    <li className="nav-item">
+                        <Link href="/posts/first-post">
+                            <span
+                                className={
+                                    router.asPath === '/posts/first-post'
+                                        ? 'nav-link active'
+                                        : 'nav-link'
+                                }
+                            >
+                                First Post
+                            </span>
+                        </Link>
+                    </li>
+                    {/* Add more navigation links as needed */}
+                </ul>
+            </div>
+        </nav >
+    );
 };
 
 export default Navigation;
